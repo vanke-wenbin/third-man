9afd3c2812a2562d6b91231a137fee67afdcd3aa
林周 <linz06@vanke.com>
2018-04-10 16:20:32 +0800

    add user

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
 const user = require('./modules/user/user.js');
 const HttpHelper = require('./helpers/http.js');
+const api = require('./constants/api');
+const UrlUtils = require('./utils/url.js');
 
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.setStorageSync('code', res.code);
+        this.getFdToken();
       }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    });
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+    // 获取用户信息
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       const code = wx.getStorageSync('code');
+    //       this.getFdToken(code);
+    //     }
+    //   }
+    // })
+  },
+  getFdToken: function () {
+    const code = wx.getStorageSync('code');
+
+    wx.getUserInfo({
+      withCredentials: true,
+      success: res => {
+        const { encryptedData, iv } = res;
+        const url = UrlUtils.getUrlWithQs(api.LOGIN, {
+          code,
+          iv,
+          provider: 'miniprogram',
+          encrypted_data: encryptedData,
+        });
+        HttpHelper.get({
+          url,
+          responseCode: 200,
+          success: data => {
+            console.log('success', data);
+          },
+          fail: err => {
+            console.log('err', err);
+          }
+        });
       }
-    })
+    });
   },
   globalData: {
     userInfo: null
