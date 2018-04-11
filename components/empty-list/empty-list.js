43528d33f276f08c681b301cbe12262c3736441e
林周 <linz06@vanke.com>
2018-04-11 13:41:47 +0800

    add empty-list

---
+++ components/empty-list/empty-list.js
@@ -0,0 +1,26 @@
+// components/bill-card/bill-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+      value: null,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/bill-list/bill-list.js
+++ pages/bill-list/bill-list.js
@@ -1,66 +1,6 @@
 Page({
   data: {
-    bills: [{
-      title: 2018,
-      data: [
-        {
-          expenses: Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 5,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 4,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 3,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 2,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 1,
-          selected: Math.random() > 0.5 ? true : false,
-        }
-      ],
-    },{
-      title: 2017,
-      data: [
-        {
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 5,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 4,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 3,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 2,
-          selected: Math.random() > 0.5 ? true : false,
-        },{
-          expenses:  Math.floor(Math.random() * 200),
-          unpaid:  Math.floor(Math.random() * 2),
-          mth: 1,
-          selected: Math.random() > 0.5 ? true : false,
-        }
-      ],
-    }]
+    bills: []
   },
   onReady: function () {
     wx.setNavigationBarTitle({
